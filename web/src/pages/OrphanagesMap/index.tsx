553d302b786ab03abc68d6aa569bc1c6dc402fb3
Matheus Santos <matheus.psantos2016@gmail.com>
2020-10-17 09:21:10 -0300

    core: app restructure

--- web/src/pages/OrphanagesMap/index.tsx
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -3,20 +3,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import Leaflet from 'leaflet';
 
 import mapMarkerImg from '../../assets/map-marker.svg'
 
-import 'leaflet/dist/leaflet.css';
+import { mapIcon } from '../../utils/mapIcon';
+
 import './styles.css';
-
-const mapIcon = Leaflet.icon({
-    iconUrl: mapMarkerImg,
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-
-    popupAnchor: [170, 2]
-})
 
 const OrphanagesMap: React.FC = () => {
   return (
@@ -56,14 +48,14 @@ const OrphanagesMap: React.FC = () => {
                     maxWidth={240}
                 >
                     Lar das meninas
-                    <Link to=''>
+                    <Link to='/orphanages/1'>
                         <FiArrowRight size={20} color='#FFF' />
                     </Link>
                 </Popup>
             </Marker>
           </Map>
         
-          <Link to='' className='create-orphanage'>
+          <Link to='/orphanages/create' className='create-orphanage'>
               <FiPlus size={32} color='#FFF' />
           </Link>
       </div>
