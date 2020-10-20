351b492d7fdc2931e060742adc80049ffb7f8244
Matheus Santos <matheus.psantos2016@gmail.com>
2020-10-19 22:34:25 -0300

    feat: Landing styled changed

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,34 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
-import { FiArrowRight } from 'react-icons/fi';
+import { Link } from "react-router-dom";
+import { FiArrowRight } from "react-icons/fi";
 
-import logoImg from '../../assets/logo.svg'
+import logoImg from "../../assets/logo.svg";
 
-import './styles.css';
+import "./styles.css";
 
 const Landing: React.FC = () => {
   return (
-      <div id="page-landing">
-          <div className="content-wrapper">
-            <img src={logoImg} alt="Happy"/>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <div className="header">
+          <img src={logoImg} alt="Happy" />
 
-            <main>
-                <h1>Leve felicidade para o mundo</h1>
-                <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-            </main>
-
-            <div className="location">
-                <strong>São Paulo</strong>
-                <span>São Paulo</span>
-            </div>
-
-            <Link to="/app" className="enter-app">
-                <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-            </Link>
+          <div className="location">
+            <strong>São Paulo</strong>
+            <span>São Paulo</span>
           </div>
+        </div>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <Link to="/login" className="login">
+            Acesso restrito
+        </Link>
+
+        <Link to="/app" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </Link>
       </div>
+    </div>
   );
-}
+};
 
 export default Landing;
